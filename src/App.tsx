8a9a7910846c543c38fdf00a8c08337d50975dd8
vienle15 <tanvien93lt@gmail.com>
2023-09-28 11:27:58 +0700

    up

--- src/App.tsx
+++ src/App.tsx
@@ -16,7 +16,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          xin chào bài 9
         </a>
       </header>
     </div>
