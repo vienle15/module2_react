f7e4ec9c5c81005d48cb8d6f21348a49c20310f7
vienle15 <tanvien93lt@gmail.com>
2023-10-10 19:15:37 +0700

    zcv

--- src/App.tsx
+++ src/App.tsx
@@ -1,43 +1,17 @@
 import React, { useEffect, useState } from "react";
 import logo from "./logo.svg";
 import "./App.css";
-import Home from "./pages/Home";
+import { getDataUser } from "./api";
 
 function App() {
   const [data, setData] = useState<any[]>([]);
-  const configValue = process.env;
-
-  console.log(1111111111, configValue);
-
-  useEffect(() => {
-    fetch("http://localhost:3000/posts")
-      .then((res) => {
-        console.log(res);
-        return res.json();
-      })
-      .then((res) => {
-        console.log("phan hoi", res);
-        setData(res);
-      })
-      .catch((err) => console.error("Kiểm tra error", err));
-    // fetchData("http://localhost:3000/posts", setData);
-  }, []);
-
+  const res = getDataUser(setData);
   return (
     <div className="App">
-      <ul>{data && data.map((item, i) => <li key={i}>{item.title}</li>)}</ul>
-
-      <Home />
+      <input type="text" onClick={(e) => handleSearch(e.target.value)} />
+      <button onClick={handleSearch}></button>
     </div>
   );
-}
-
-function fetchData(url: string, setData: Function) {
-  fetch(url)
-    .then((res) => res.json())
-    .then((res) => {
-      setData(res);
-    });
 }
 
 export default App;
---
+++ src/api/common.ts
@@ -0,0 +1,2 @@
+export const GET_DATA_USER = process.env.REACT_APP_API;
+export const SET_DATA_USER = process.env.REACT_APP_API;
